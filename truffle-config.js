50c82ea97a6c2b754287af44f8503dbc22bbcb78
Im-Mr-Chris <mr.christophercelaya@gmail.com>
2022-01-15 06:52:53 -0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,62 +1,17 @@
-require('babel-register');
-require('babel-polyfill');
-require('dotenv').config();
 const HDWalletProvider = require("@truffle/hdwallet-provider")
+require('dotenv').config(); // Load .env file
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
-    matic: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc-mumbai.maticvigil.com/v1/bd76d9f2e2c3993bbfef0082f25c30d017fc1feb`),
-      network_id: 80001,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true,
-      gas: 6000000,
-      gasPrice: 10000000000,
-    },
-    rinkeby: {
-      provider: function () {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_RINKEBY
-        )
-      },
-      network_id: 4,
-      gas: 6000000,
-      gasPrice: 10000000000,
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_ROPSTEN
-        )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: "*"
-    },
-    goerli: {
-      provider: function () {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_GOERLI
-        )
-      },
-      network_id: 5,
-      networkCheckTimeout: 1000000,
-      timeoutBlocks: 200,
-      gas: 5000000,
-      gasPrice: 25000000000
+   // For Ganache, your personal blockchain
+   development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port 
+      network_id: "*",       // Any network (default: none)
     },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: './src/contracts/', // path to Smart Contracts
+  contracts_build_directory: './src/abis/', // Path to ABIs
   compilers: {
     solc: {
       optimizer: {
